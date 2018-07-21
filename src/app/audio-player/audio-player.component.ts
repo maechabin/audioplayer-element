53c5f609095fff8b8c1c144fac169c7a87668f3e
maechabin <mitoppo@gmail.com>
2018-07-22 03:08:20 +0900

    wip: 曲の再生停止まで実装

--- src/app/audio-player/audio-player.component.ts
+++ src/app/audio-player/audio-player.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { AudioPlayerService } from './audio-player.service';
 
 @Component({
@@ -6,13 +7,40 @@ import { AudioPlayerService } from './audio-player.service';
   templateUrl: './audio-player.component.html',
   styleUrls: ['./audio-player.component.css'],
 })
-export class AudioPlayerComponent {
-  constructor(private audioPlayerService: AudioPlayerService) {}
-  handleStopButton() {
-    this.audioPlayerService.stop();
+export class AudioPlayerComponent implements OnInit, OnChanges {
+  private _files = new BehaviorSubject(this.files);
+
+  @Input()
+  set files(files: string) {
+    const filesArray = JSON.parse(files);
+    this._files.next(filesArray);
   }
+
+  isEnded = this.audioPlayerService.isEnded;
+
+  constructor(private audioPlayerService: AudioPlayerService) {}
+
+  ngOnInit() {
+    // this._files.subscribe(files => {
+    //   console.log(files);
+    //   this.audioPlayerService.audioFiles = files;
+    // });
+  }
+
+  ngOnChanges() {}
 
   handlePlayButton() {
     this.audioPlayerService.play();
+    this.isEnded = this.audioPlayerService.isEnded;
+  }
+
+  handlePauseButton() {
+    this.audioPlayerService.pause();
+    this.isEnded = this.audioPlayerService.isEnded;
+  }
+
+  handleStopButton() {
+    this.audioPlayerService.stop();
+    this.isEnded = this.audioPlayerService.isEnded;
   }
 }
--- src/app/audio-player/audio-player.service.ts
+++ src/app/audio-player/audio-player.service.ts
@@ -4,11 +4,44 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AudioPlayerService {
+  audio = new Audio();
+
+  private _audioFiles: string[] = [];
+
+  set audioFiles(files: string[]) {
+    this._audioFiles = files;
+  }
+  get audioFiles(): string[] {
+    return this._audioFiles;
+  }
+
+  get isEnded(): boolean {
+    return this.audio.ended;
+  }
+
+  constructor() {
+    this.audio.src = 'https://mae.chab.in/wp-content/uploads/music/mm48.mp3';
+  }
+
+  addAudio(files: string[]) {
+    const audioFiles = this.audioFiles;
+    return audioFiles.concat(files);
+  }
+
+  clearAllAudio() {
+    this.audioFiles = [];
+  }
+
   play() {
-    //
+    this.audio.play();
+  }
+
+  pause() {
+    this.audio.pause();
   }
 
   stop() {
-    //
+    this.audio.pause();
+    this.audio.currentTime = 0;
   }
 }
